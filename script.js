4fa777e8152a21b491d21744a02e0cf5aa72ea46
David <david.kjellstrand@gmail.com>
2024-11-15 15:08:08 +0100

    added increase & decrease buttons and changes to printProducts

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const products = [
+//---------------- HTML ELEMENTS ------------
+
+const productsContainer = document.querySelector('#productsContainer');
+
+//---------------- HTML ELEMENTS ------------
+
+const product = [
     {
         id: 0,
         name: 'Product 1',
         price: 10,
         rating: 4,
-        ammount: 0,
+        amount: 0,
         category: 'Sweet',
         img: {
             url: 'assets/images/cookie.jpg',
@@ -18,7 +24,7 @@ const products = [
         name: 'Product 2',
         price: 15,
         rating: 2,
-        ammount: 0,
+        amount: 0,
         category: 'Salty',
         img: {
             url: 'assets/images/cookie.jpg',
@@ -29,25 +35,72 @@ const products = [
     },
 ];
 
-//---------------- HTML ELEMENTS ------------
-
-const productsListDiv = document.querySelector('#product-list');
-
 //---------------- PRINT PRODUCT IN HTML ------------
 
-function printProductList() {
-    // Rensa först
-    productsListDiv.innerHTML = '';
+function printProducts() {
+    // RENSA
+    productsContainer.innerHTML = '';
 
-    products.forEach(product => {
-        productsListDiv.innerHTML += `
-        <article class="product">
-        <h3>${product.name}<h3>
-        <p>${product.price} kr</p>
-        <p>Rating:${product.rating}</p>
-        <img src="${product.img.url}" alt="${product.img.alt}">
+    // PRINT HTML - För varje product - printa en articel med innehåll i den tomma div'en 
+    product.forEach(product => {
+        productsContainer.innerHTML += `
+            <article class="product">
+                <h3>${product.name}</h3>
+                <img src="${product.img.url}" alt="${product.img.alt}">
+                <div>Price: <span>${product.price}<span> kr</div>
+                <div>Rating: <span>${product.rating}</span></div>
+                <div>
+                    <button class="decrease" id="decrease-${product.id}"> - </button>
+                    <input type="number" min="0" value="${product.amount}" id="input-${product.id}">
+                    <button class="increase" id="increase-${product.id}"> + </button>
+                </div>
+            </article>
         `;
+    });
+
+    // INCREASE BTN w. CLICK EVENT
+    const increaseButtons = document.querySelectorAll('button.increase');
+    increaseButtons.forEach(button => {
+        button.addEventListener('click', increaseProductCount);
+    });
+
+    const decreaseButtons = document.querySelectorAll('button.decrease');
+    decreaseButtons.forEach(button => {
+        button.addEventListener('click', decreaseProductCount);
     });
 }
 
-printProductList();
+printProducts();
+
+// INCREASE PRODUCT 
+
+function increaseProductCount(e) {
+    const productId = Number(e.target.id.replace('increase-', ''));
+    // Leta rätt på produkten i arrayen som har det id:t
+    const foundProductIndex = product.findIndex(product => product.id === productId);
+    // Om produkten inte finns, skriv ut felmeddelande i consolen och avbryt att resten av koden körs med "return"
+    if (foundProductIndex === -1) {
+        console.error('Det finns ingen sådan produkt i produktlistan! Kolla att id:t är rätt.');
+        return;
+    }
+    product[foundProductIndex].amount += 1; // öka dess amount med +1
+    printProducts();
+}
+
+// DECREASE PRODUCT 
+
+function decreaseProductCount(e) {
+    const productId = Number(e.target.id.replace('decrease-', ''));
+    // Leta rätt på produkten i arrayen som har det id:t
+    const foundProductIndex = product.findIndex(product => product.id === productId);
+    // Om produkten inte finns, skriv ut felmeddelande i consolen och avbryt att resten av koden körs med "return"
+    if (foundProductIndex === -1) {
+        console.error('Det finns ingen sådan produkt i produktlistan! Kolla att id:t är rätt.');
+        return;
+    }
+    if (product[foundProductIndex].amount == 0) { // Om siffran är 0, gör inget!
+        return;
+    }
+    product[foundProductIndex].amount -= 1; // minska dess amount med -1
+    printProducts();
+}
