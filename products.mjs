1320627497405207ef16f67fa0b6c0bd7d2d11c5
David <david.kjellstrand@gmail.com>
2024-11-27 18:04:12 +0100

    added more products

--- products.mjs
+++ products.mjs
@@ -55,6 +55,118 @@ const product = [
             alt: 'Chocolate chip cookie'
         },
     },
+    {
+        id: 4,
+        name: 'Triple Choco Crumb',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 5,
+        name: '"Classic Cookie"',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 6,
+        name: 'White Choco Crisp',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 7,
+        name: 'Fudgy Flake',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 8,
+        name: 'Oats With The Most',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 9,
+        name: 'Cookie Mix 5',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 10,
+        name: 'Cookie Mix 10',
+        price: 35,
+        rating: 3.5,
+        amount: 0,
+        category: 'Salty',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
+    {
+        id: 11,
+        name: 'Cookie Mix 15',
+        price: 650,
+        rating: 5,
+        amount: 0,
+        category: 'Sweet',
+        img: {
+            url: 'assets/images/cookie.jpg',
+            width: 552,
+            height: 552,
+            alt: 'Chocolate chip cookie'
+        },
+    },
 ];
 
 export default product;
